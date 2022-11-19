7376a584760b12368de6e0d90be726aa3467e55f
sinji2102 <sinzi031012@naver.com>
2022-11-20 00:29:07 +0900

    Add: open_close file

--- chopstick_detection/chopstick_detection.py
+++ chopstick_detection/chopstick_detection.py
@@ -5,7 +5,7 @@ cap = cv.VideoCapture("C:/Users/sinji/Desktop/firstick/src/wood05.mp4")
 cnt = 1
 
 # 젓가락 전체 길이와 젓가락 쥔 앞 부분 길이 확인해서 1/5 지점 이면 True를 출력하는 코드 >> 값 수정 + 1/5 지점에 점 찍는 것까지
-while True :    
+while True :
 
     ret, frame = cap.read()
     resize = cv.resize(frame, (0,0), fx = 0.5, fy = 0.5, interpolation = cv.INTER_AREA)
@@ -33,7 +33,7 @@ while True :
         for i in line :
             (a11, b11, a12, b12) = i[0]
             if abs(b11 - b12) < 70 :
-                cv.line(resize, (a11, b11), (a12, b12), (0, 0, 255), 2)
+                # cv.line(resize, (a11, b11), (a12, b12), (0, 0, 255), 2)
                 whole = a12 - a11
 
     # 젓가락 일부(손가락 부분 제거하고) 출력 / 노란색
@@ -41,7 +41,7 @@ while True :
         for i in lines :
             (a21, b21, a22, b22) = i[0]
             if abs((b22 - b21)/(a22 - a21)) < 5 :
-                # cv.line(resize, (a21, b21), (a22, b22), (0, 255, 255), 2)
+                cv.line(resize, (a21, b21), (a22, b22), (255, 0, 255), 2)
                 part = a22 - a21
 
     # 점 찍기
@@ -49,12 +49,15 @@ while True :
         if whole * 0.55 < part < whole * 0.7 :
             print("True", cnt)
             cnt += 1
-            cv.line(resize, (a22, b22), (a22, b22), (255, 0, 0), 15)
+            # cv.line(resize, (a22, b22), (a22, b22), (255, 0, 0), 15)
 
     cv.imshow("resize", resize)
     cv.imshow("canny", canny)
+    cv.imshow("canny_",canny_)
     # cv.imshow("dst", dst)
 
     key = cv.waitKey(5)
     if key == 27 :
         break
+
+# 0819_03.py까지 커밋 완료
